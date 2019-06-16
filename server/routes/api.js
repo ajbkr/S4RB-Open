5fee1d6a5f543ceb74e89f195d08158e99f4305c
Andrew Baker <andrew_j_baker2@hotmail.com>
2019-06-16 17:45:31 +0100

    Extend API endpoint to include CPMU

--- server/routes/api.js
+++ server/routes/api.js
@@ -10,13 +10,39 @@ router.get('/complaints/csv', (req, res) => {
 })
 
 router.get('/complaints/json', (req, res) => {
-  const data = fs.readFileSync(path.resolve(__dirname, '../../data/complaints.csv'), { encoding: 'utf8' })
+  const data = fs.readFileSync(
+    path.resolve(__dirname, '../../data/complaints.csv'),
+    { encoding: 'utf8' }
+  )
   const options = {
     delimiter: ',',
     quote: '"'
   }
 
   res.json(csvjson.toObject(data, options))
+})
+
+router.get('/cpmu/json', (req, res) => {
+  const data = fs.readFileSync(
+    path.resolve(__dirname, '../../data/complaints.csv'),
+    { encoding: 'utf8' }
+  )
+  const options = {
+    delimiter: ',',
+    quote: '"'
+  }
+
+  const complaints = csvjson.toObject(data, options)
+  const cpmu = complaints.map(
+    ({ Month: month, Complaints: complaints, UnitsSold: unitsSold }) => ({
+      CPMU: (complaints / unitsSold * 1000000).toFixed(5),
+      Month: new Date(month).toLocaleDateString('en-GB', {
+        month: 'long',
+        year: 'numeric'
+      })
+    })
+  )
+  res.json(cpmu)
 })
 
 module.exports = router
